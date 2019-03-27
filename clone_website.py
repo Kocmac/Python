24e0a1159b921f1aa962acb873f01f82d748040d
Kocmac <47703457+Kocmac@users.noreply.github.com>
2019-03-27 22:04:08 +0200

    Update clone_website.py

--- clone_website.py
+++ clone_website.py
@@ -1,9 +1,21 @@
 import urllib.request
+import urllib.error
 url = urllib.request.Request (input('Paste the url you want to copy: '))
-with urllib.request.urlopen(url) as response:
-   char_set = response.headers.get_content_charset()
-   html = response.read().decode(char_set)
-file_name = input("Give the name of the file to be created: ")
-file_format = input("Give the file formating (e.x. html, txt): ")
-with open (file_name+"."+file_format, "w", encoding = char_set) as p:
-   p.write(html)
+try:
+   with urllib.request.urlopen(url) as response:
+      char_set = response.headers.get_content_charset()
+      html = response.read().decode(char_set)
+   file_name = input("Give the name of the file to be created: ")
+   file_format = input("Give the file formating (e.x. html, txt): ")
+   with open (file_name+"."+file_format, "w", encoding = char_set) as p:
+      p.write(html)
+except urllib.HTTPError as e:
+      print(e.code)
+      print("HTTP Error")
+except urllib.error.URLError as e:
+   if hasattr(e, 'reason'):
+      print('Connection Server Error')
+      print("Due to :", e.reason)
+else:
+   print("--- {} created ---".format(file_name+"."+file_format))
+   
